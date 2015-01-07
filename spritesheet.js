9b3b7697cb5867675f37577db1c8cfc0f3865d94
TALENTICA-ALL\vijayy <vijayyadav1002@gmail.com>
2015-01-07 13:12:20 +0530

    Rendering Texturepacker Output unmodified

--- spritesheet.js
+++ spritesheet.js
@@ -20,48 +20,41 @@ SpriteSheetClass = Class.extend({
     //
     // The Image object that we created for our
     // atlas.
-	img: null,
+    img: null,
 
     // The URL path that we grabbed our atlas
     // from.
-	url: "grits_effects.png",
+    url: "",
 
     // An array of all the sprites in our atlas.
-	sprites: new Array(),
+    sprites: [],
 
-	//-----------------------------------------
-	init: function () {
-        this.img = new Image();
-        that = this;
-        this.img.onload = function(){
-            that.load(that.url);
-        };
-        this.img.src=this.url;
-    },
+    //-----------------------------------------
+    init: function () {},
 
-	//-----------------------------------------
+    //-----------------------------------------
     // Load the atlas at the path 'imgName' into
     // memory. This is similar to how we've
     // loaded images in previous units.
-	load: function (imgName) {
-		// Store the URL of the spritesheet we want.
+    load: function (imgName) {
+        // Store the URL of the spritesheet we want.
         this.url = imgName;
         
         // Create a new image whose source is at 'imgName'.
-		var img = new Image();
-		img.src = imgName;
+        var img = new Image();
+        img.src = imgName;
 
         // Store the Image object in the img parameter.
-		this.img = img;
+        this.img = img;
 
         // Store this SpriteSheetClass in our global
         // dictionary gSpriteSheets defined above.
-		gSpriteSheets[imgName] = this;
-	},
+        gSpriteSheets[imgName] = this;
+    },
 
-	//-----------------------------------------
-	// Define a sprite for this atlas
-	defSprite: function (name, x, y, w, h, cx, cy) {
+    //-----------------------------------------
+    // Define a sprite for this atlas
+    defSprite: function (name, x, y, w, h, cx, cy) {
         // We create a new object with:
         //
         // The name of the sprite as a string
@@ -77,67 +70,146 @@ SpriteSheetClass = Class.extend({
         // so we don't have to do the calculations
         // each time we need this. This might seem
         // minimal, but it adds up!
-		var spt = {
-			"id": name,
-			"x": x,
-			"y": y,
-			"w": w,
-			"h": h,
-			"cx": cx == null ? 0 : cx,
-			"cy": cy == null ? 0 : cy
-		};
+        var spt = {
+            "id": name,
+            "x": x,
+            "y": y,
+            "w": w,
+            "h": h,
+            "cx": cx === null ? 0 : cx,
+            "cy": cy === null ? 0 : cy
+        };
 
         // We push this new object into
         // our array of sprite objects,
         // at the end of the array.
-		this.sprites.push(spt);
-	},
+        this.sprites.push(spt);
+    },
 
-	//-----------------------------------------
+    //-----------------------------------------
     // Parse the JSON file passed in as 'atlasJSON'
     // that is associated to this atlas.
-	parseAtlasDefinition: function (atlasJSON) {
+    parseAtlasDefinition: function (atlasJSON) {
         // Parse the input 'atlasJSON' using the
         // JSON.parse method and store it in a
         // variable.
-        //
-		// YOUR CODE HERE
-         var atlas = JSON.parse(atlasJSON);
+
+        var parsed = JSON.parse(atlasJSON);
 
         // For each sprite in the parsed JSON,
         // 'chaingun.png', chaingun_impact.png',
-        // etc.:
-        //
-        // 1) Calculate the center (x,y) offsets
-        //    from the 'width' and 'height'
-        //    values listed in the JSON.
-        //    Note that these are an offset
-        //    FROM the 'width' and 'height',
-        //    so they will be negative!
-        //
-        // 2) Pass the sprite name, the (x,y)
-        //    coordinates, the width and height,
-        //    and the center (x,y) offsets
-        //    you just calculated to the above
-        //    'defSprite' method of our
-        //    'SpriteSheetClass'.
-        //
-        // YOUR CODE HERE
-        var frames = atlasJSON.frames,
-            name, x, y, width, height, cx, cy;
-         for(var frame in frames) {
-            if(frames.hasOwnProperty(frame)){
-                name = frame;
-                x = frames.frame.x;
-                y = fralmes.frame.y;
-                width = frames.frame.w;
-                height = frames.frame.h;
-                cx = -Math.floor(width/2);
-                cy = -Math.floor(height/2);
-                this.defSprite(name, x, y, width, height, cx, cy);
-            }
+        // etc....
+        for(var key in parsed.frames) {
+            // Grab the sprite from the parsed JSON...
+            var sprite = parsed.frames[key];
+
+            // Define the center of the sprite as an offset
+            // (hence the negative).
+            // We don't want to have to calculate these
+            // values every single time we want to draw a
+            // sprite! It adds up!
+            var cx = -sprite.frame.w * 0.5;
+            var cy = -sprite.frame.h * 0.5;
+
+            // Define the sprite for this sheet by calling
+            // defSprite to store it into the 'sprites' Array.
+            this.defSprite(key, sprite.frame.x, sprite.frame.y, sprite.frame.w, sprite.frame.h, cx, cy);
         }
-	}
+    },
+
+    //-----------------------------------------
+    // Walk through all the sprite definitions for this
+    // atlas, and find which one matches the name.
+    getStats: function (name) {
+        // For each sprite in the 'sprites' Array...
+        for(var i = 0; i < this.sprites.length; i++) {
+            
+            // Check if the sprite's 'id' parameter
+            // equals the passed in name...
+            if(this.sprites[i].id === name) {
+                // and return that sprite if it does.
+                return this.sprites[i];
+            }
+
+        }
+
+        // If we don't find the sprite, return null.
+        return null;
+    }
 
 });
 
+//-----------------------------------------
+// External-facing function for drawing sprites based
+// on the sprite name (ie. "chaingun.png", and the
+// position on the canvas to draw to.
+function drawSprite(spritename, posX, posY) {
+    // Walk through all our spritesheets defined in
+    // 'gSpriteSheets' and for each sheet:
+    //
+    // 1) Use the getStats method of the spritesheet
+    //    to find if a sprite with name 'spritename'
+    //    exists in that sheet.
+    //
+    // 2) If we find the appropriate sprite, call
+    //    '__drawSpriteInternal' with parameters as
+    //    described below.
+    //
+    // YOUR CODE HERE
+
+}
+
+//-----------------------------------------
+// External-facing function for drawing sprites based
+// on the sprite object stored in the 'sprites Array,
+// the 'SpriteSheetClass' object stored in the
+// 'gSpriteSheets' dictionary, and the position on
+// canvas to draw to.
+function __drawSpriteInternal(spt, sheet, posX, posY) {
+    // First, check if the sprite or sheet objects are
+    // null.
+    //
+    // YOUR CODE HERE
+    
+    
+    // Call the drawImage method of our canvas context
+    // using the full drawImage API. drawImage takes,
+    // in order:
+    //
+    // 1) the Image object to draw, this is our entire
+    //    spritesheet.
+    //
+    // 2) the x-coordinate we are drawing from in the
+    //    spritesheet.
+    //
+    // 3) the y-coordinate we are drawing from in the
+    //    spritesheet.
+    //
+    // 4) the width of the sprite we are drawing from
+    //    our spritesheet.
+    //
+    // 5) the height of the sprite we are drawing from
+    //    our spritesheet.
+    //
+    // 6) the x-coordinate we are drawing to in our
+    //    canvas.
+    //
+    // 7) the y-coordinate we are drawing to in our
+    //    canvas.
+    //
+    // 8) the width we are drawing in our canvas. This
+    //    is in case we want to scale the image we are
+    //    drawing to the canvas. In our case, we don't.
+    //
+    // 9) the height we are drawing in our canvas. This
+    //    is in case we want to scale the image we are
+    //    drawing to the canvas. In our case, we don't.
+    //
+    // Wow, that's a lot of parameters. Luckily, most
+    // of them are stored directly in the sprite object
+    // we want to draw.
+    //
+    // YOUR CODE HERE
+    
+    
+}
